4994b65b3d23eb10c98e6e09b663ff6f8154b005
Francisco <fvcneto.master98@gmail.com>
2018-07-06 21:43:36 -0300

    set some timeline functions to the measures file

--- dataset/measures.py
+++ dataset/measures.py
@@ -1,6 +1,8 @@
+from timeline import get_dates, get_datetimes
 import numpy as np
 
-def days_of_week(dates):
+def days_of_week(timeline):
+    dates = get_dates(timeline)
     week_list = [0,0,0,0,0,0,0]
     for date in dates:
         week_list[date] += 1
@@ -14,14 +16,13 @@ def tweets_in_week(values):
     return number_tweets
 
 # Get the intevals from one tweet to another
-def get_intervals(datetimes):
+def get_intervals(timeline):
+    datetimes = get_datetimes(timeline)
     interval_list = []
     for i in range(len(datetimes)-1, 0, -1):
         interval = datetimes[i] - datetimes[i - 1]
         interval_list.append(abs(interval.total_seconds()))
-
-    print('IW:', str(len(interval_list)))
-
+        
     lim_sup, lim_inf = limit_iqr(interval_list)
     intervals = eliminate_outliers((lim_sup, lim_inf), interval_list)
 
